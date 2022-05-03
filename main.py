c098431683707937d7c28f003d3d827cdfe57f8a
BigBoyFancyPants <BigBoyFancyPants@users.noreply.github.com>
2022-05-03 15:51:40 +1000

    Concurrent check funciton complet. Minor edits to Diary list format and menu loop printing. Constants added to make Diary handeling easier

--- main.py
+++ main.py
@@ -2,8 +2,75 @@
 ##import calls##
 import time
 ################
+##Global Variables##
+#                 10        20        30        40 
+#       012345678901234567890123456789012345678901234567890123
+Diary=["HIGH  ;10/06/2022;07;22;Testing entry one             ",
+       "MEDIUM;01/09/2022;12;22;Testing entry two             ",
+       "LOW   ;20/07/2023;10;15;Testing entry three           "]
+##constants##
+PRIORITY_START = 0
+PRIORITY_END = 6
+DATE_START = 7
+DATE_END = 17
+STARTTIME_START = 18
+STARTTIME_END = 20
+ENDTIME_START = 21
+ENDTIME_END = 23
+DISCRIPTION_START = 24
+DISCRIPTION_END = 44
+DAY_START = 0
+DAY_END = 2
+MONTH_START = 3
+MONTH_END = 5
+YEAR_START = 6
+YEAR_END = 10
+####################
 
-
+####################################################
+# IS_CONCURRENT_APPOINTMENT(start,end,date)
+# ##########	
+# inputs:
+#   start, int:
+#       The hour of the start time (validated)
+#   end, int:
+#       The hour of the end time (validated)
+#   date, str[10],("dd/mm/yyyy"):
+#       The date input from the user (validated)
+# outputs:
+# 	(unnamed), bool:
+#       Wether of not the apointment is concurrent as a True/False boolean
+#           If there is an apointment overlap returns True. No overlap = False
+# ##########
+#   Searches through Diary[] and checks for overlappong apointments between the
+#   new entry and current entries. Requirse a global array of strings named Diary 
+#   formated "[6];[10];[2];[2];[30]" (e.g "High  ;23/09/2022;09;10;CSC1401 class                 ")
+# ##########
+# Author: Timothy van den Bosch
+# Date: 03/05/2022
+# History
+# 	Rev 1.0: 03/05/2022, Timothy van den Bosch
+#        Completed function
+####################################################
+def IS_CONCURRENT_APPOINTMENT(start, end, date):
+    #first check for same day entries. If the new event dosn't share a day there is no overlap
+    NDay = int(date[DAY_START:DAY_END])
+    NMonth = int(date[MONTH_START:MONTH_END])
+    NYear = int(date[YEAR_START:YEAR_END])
+    for entry in Diary: 
+        EDate = entry[DATE_START:DATE_END]
+        EDay = int(EDate[DAY_START:DAY_END])
+        EMonth = int(EDate[MONTH_START:MONTH_END])
+        EYear = int(EDate[YEAR_START:YEAR_END])
+        if EYear == NYear and EMonth == NMonth and EDay == NDay:#is the same day
+            #check for time overlap
+            EStart = int(entry[STARTTIME_START:STARTTIME_END])
+            EEnd = int(entry[ENDTIME_START:ENDTIME_END])
+            if (EStart<=start<EEnd) or (start<=EStart<end):
+                print("Warning!\n\tThe new entry overlaps with the current entry: "+entry[24:44]+":\n\t"+entry[18:20]+" to "+entry[21:23]+" on "+entry[7:17])
+                return True
+    print("No overlaps detected!")
+    return False #no overlaps
 ####################################################
 # VALIDATE_TIME(start,end,date)
 # ##########	
@@ -32,9 +99,9 @@ def VALIDATE_TIME(start, end, date):
     if not (start>=7 and end <=22 and start<end):
         print("Time not valid: Start and/or end times do not meet conditions")
         return False
-    DDay = int(date[0:2])-time.localtime().tm_mday
-    DMonth = int(date[3:5])-time.localtime().tm_mon
-    DYear = int(date[6:10])-time.localtime().tm_year
+    DDay = int(date[DAY_START:DAY_END])-time.localtime().tm_mday
+    DMonth = int(date[MONTH_START:MONTH_END])-time.localtime().tm_mon
+    DYear = int(date[YEAR_START:YEAR_END])-time.localtime().tm_year
     DStart = start-time.localtime().tm_hour
     if DYear > 0:
         return True
@@ -219,20 +286,15 @@ def GET_PRIORITY():
             break
         print("Invalid input. Please try again")
     return PRIORITY
+                       
 
 
-
-                              
-
-Diary=[]
-Diary.append({"date": "23/05/1987", "StartTime": int(10), "EndTime": int(12), "Description": 'My B\'day','priority': 'High'})
-print(Diary[0]["date"])
 menu=True
 while menu:
     if len(Diary) > 0:
         for entry in Diary:
             print('\n')
-            print('Appointment Info: \n' + 'Date: ' +  entry['date' ], 'Start Time: ' + str(entry['StartTime']), 'End Time: ' + str(entry['EndTime']), entry['Description'], entry['priority'] )
+            print('Appointment Info: \n' + 'Date: ' +  entry[DATE_START:DATE_END], 'Start Time: ' + entry[STARTTIME_START:STARTTIME_END], 'End Time: ' + entry[ENDTIME_START:ENDTIME_END], entry[DISCRIPTION_START:DISCRIPTION_END], entry[PRIORITY_START:PRIORITY_END] )
     else:
         print('\n Select Add new record to create a new appointment')        
     print("""
